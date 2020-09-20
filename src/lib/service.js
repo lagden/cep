9b6069b799dfc6ac93245147cb7503c5e87bb22c
Thiago Lagden <lagden@gmail.com>
2020-09-20 14:37:12 -0300

    update deps and devDeps

--- src/lib/service.js
+++ src/lib/service.js
@@ -1,6 +1,6 @@
 'use strict'
 
-// const {inspect} = require('util')
+const {inspect} = require('util')
 const {createClientAsync} = require('soap')
 const CepError = require('./cep-error')
 const dict = require('./dict')
@@ -15,7 +15,7 @@ async function service(method, args) {
 	try {
 		const client = await createClientAsync(wsdl.producao)
 		const _response = await client[`${method}Async`](args, {method: 'POST'})
-		// console.log(inspect(_response, false, null, true))
+		console.log(inspect(_response, false, null, true))
 		const [{return: response}] = _response
 		response.success = true
 		response.status = 200
